5b9a2d3d9ec15993271e56d934437f133ed948da
Geethu Krishna <geethu.krishna@ccsdev01.onmicrosoft.com>
2026-01-14 11:16:13 +0530

    minor

--- QatarCement/src/webparts/rfqReview/interfaces/IRfqReviewProps.ts
+++ QatarCement/src/webparts/rfqReview/interfaces/IRfqReviewProps.ts
@@ -89,7 +89,7 @@ export interface IRfqReviewState {
   vendorTermsAndConditions: string;
   vendorTechnicalSupport: string;
   vendorWarrantySupport: string;
-}
+} 
 
 export interface IItemData {
   index: number;
